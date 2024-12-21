1e2dcb8f0683ef9f1d4f5b8791768d967e4d5506
pruthvesh <code.pruthvesh@gmail.com>
2024-12-21 17:50:14 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,196 @@
+import axios from "axios";
+import { HttpsProxyAgent } from "https-proxy-agent";
+
+const RE_YOUTUBE =
+  /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/i;
+const USER_AGENT =
+  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36,gzip(gfe)';
+const RE_XML_TRANSCRIPT =
+  /<text start="([^"]*)" dur="([^"]*)">([^<]*)<\/text>/g;
+
+export class YoutubeTranscriptError extends Error {
+  constructor(message:string) {
+    super(`[YoutubeTranscript] 🚨 ${message}`);
+  }
+}
+
+export class YoutubeTranscriptTooManyRequestError extends YoutubeTranscriptError {
+  constructor() {
+    super(
+      'YouTube is receiving too many requests from this IP and now requires solving a captcha to continue'
+    );
+  }
+}
+
+export class YoutubeTranscriptVideoUnavailableError extends YoutubeTranscriptError {
+  constructor(videoId: string) {
+    super(`The video is no longer available (${videoId})`);
+  }
+}
+
+export class YoutubeTranscriptDisabledError extends YoutubeTranscriptError {
+  constructor(videoId: string) {
+    super(`Transcript is disabled on this video (${videoId})`);
+  }
+}
+
+export class YoutubeTranscriptNotAvailableError extends YoutubeTranscriptError {
+  constructor(videoId: string) {
+    super(`No transcripts are available for this video (${videoId})`);
+  }
+}
+
+export class YoutubeTranscriptNotAvailableLanguageError extends YoutubeTranscriptError {
+  constructor(lang: string, availableLangs: string[], videoId: string) {
+    super(
+      `No transcripts are available in ${lang} this video (${videoId}). Available languages: ${availableLangs.join(
+        ', '
+      )}`
+    );
+  }
+}
+
+export interface TranscriptConfig {
+  lang?: string;
+  proxyUrl?:string;
+}
+export interface TranscriptResponse {
+  text: string;
+  duration: number;
+  offset: number;
+  lang?: string;
+}
+
+/**
+ * Class to retrieve transcript if exist
+ */
+export class YoutubeTranscript {
+  /**
+   * Fetch transcript from YTB Video
+   * @param videoId Video url or video identifier
+   * @param config Get transcript in a specific language ISO
+   */
+  public static async fetchTranscript(
+    videoId: string,
+    config?: TranscriptConfig
+  ): Promise<TranscriptResponse[]> {
+    const identifier = this.retrieveVideoId(videoId);
+    const proxyAgent = (config?.proxyUrl) ? new HttpsProxyAgent(config.proxyUrl): null;
+
+    const videoPageResponse = await axios.get(
+      `https://www.youtube.com/watch?v=${identifier}`,
+      {
+        headers: {
+          ...(config?.lang && { 'Accept-Language': config.lang }),
+          'User-Agent': USER_AGENT,
+        },
+        httpAgent: proxyAgent
+      }
+    );
+    const videoPageBody = await videoPageResponse.data;
+    // console.log(videoPageBody,"vpb")
+    const splittedHTML = videoPageBody.split('"captions":');
+
+    if (splittedHTML.length <= 1) {
+      if (videoPageBody.includes('class="g-recaptcha"')) {
+        throw new YoutubeTranscriptTooManyRequestError();
+      }
+      if (!videoPageBody.includes('"playabilityStatus":')) {
+        throw new YoutubeTranscriptVideoUnavailableError(videoId);
+      }
+      throw new YoutubeTranscriptDisabledError(videoId);
+    }
+
+    const captions = (() => {
+      try {
+        return JSON.parse(
+          splittedHTML[1].split(',"videoDetails')[0].replace('\n', '')
+        );
+      } catch (e) {
+        return undefined;
+      }
+    })()?.['playerCaptionsTracklistRenderer'];
+
+    if (!captions) {
+      throw new YoutubeTranscriptDisabledError(videoId);
+    }
+
+    if (!('captionTracks' in captions)) {
+      throw new YoutubeTranscriptNotAvailableError(videoId);
+    }
+
+    if (
+      config?.lang &&
+      !captions.captionTracks.some(
+        (track:any) => track.languageCode === config?.lang
+      )
+    ) {
+      throw new YoutubeTranscriptNotAvailableLanguageError(
+        config?.lang,
+        captions.captionTracks.map((track:any) => track.languageCode),
+        videoId
+      );
+    }
+
+    const transcriptURL = (
+      config?.lang
+        ? captions.captionTracks.find(
+            (track:any) => track.languageCode === config?.lang
+          )
+        : captions.captionTracks[0]
+    ).baseUrl;
+
+    const transcriptResponse = await axios.get(transcriptURL, {
+      headers: {
+        ...(config?.lang && { 'Accept-Language': config.lang }),
+        'User-Agent': USER_AGENT,
+      },
+      httpAgent: proxyAgent,
+    });
+    if (transcriptResponse.status != 200) {
+      throw new YoutubeTranscriptNotAvailableError(videoId);
+    }
+    const transcriptBody = await transcriptResponse.data;
+    const results = [...transcriptBody.matchAll(RE_XML_TRANSCRIPT)];
+    return results.map((result) => ({
+      text: result[3],
+      duration: parseFloat(result[2]),
+      offset: parseFloat(result[1]),
+      lang: config?.lang ?? captions.captionTracks[0].languageCode,
+    }));
+  }
+
+  public static getCompleteTranscript(transcriptDetails:TranscriptResponse[]){
+    let transcript = ""
+    transcriptDetails.forEach((transcriptDetail)=>{
+        transcript += transcriptDetail.text + " "
+    })
+    return transcript;
+  }
+
+  /**
+   * Retrieve video id from url or string
+   * @param videoId video url or video id
+   */
+  private static retrieveVideoId(videoId: string) {
+    if (videoId.length === 11) {
+      return videoId;
+    }
+    const matchId = videoId.match(RE_YOUTUBE);
+    if (matchId && matchId.length) {
+      return matchId[1];
+    }
+    throw new YoutubeTranscriptError(
+      'Impossible to retrieve Youtube video ID.'
+    );
+  }
+}
+
+const getTranscripts = async ()=>{
+  const transcriptResponse = await YoutubeTranscript.fetchTranscript("6qOzZs-ljHQ",{proxyUrl: "http://RVdxY9NQchMk:BNphEeFXYY8u@superproxy.zenrows.com:1337"})
+  console.log(transcriptResponse)
+  const transcriptText = YoutubeTranscript.getCompleteTranscript(transcriptResponse)
+  console.log(transcriptText)
+}
+
+getTranscripts()
